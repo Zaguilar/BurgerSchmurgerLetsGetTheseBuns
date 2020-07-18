0f37fb98440129197869a994ac70cffdb70c1377
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 09:22:48 -0500

    schema file configured and added

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+Drop DATABASE IF EXISTS burgers_db
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT,
+  burger_name varchar (300),
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+);
+​-- Insert a set of records.
