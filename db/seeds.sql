1fdc3329581999da589c78ce2176135186b028f1
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 09:23:56 -0500

    seeds file configured with four entries

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Teriyaki Pineapple Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hatch Green Chili Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", true);
