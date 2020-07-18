3d2ac6c254dcd98ede228e4f1c58cfc3c26901e1
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 18:47:50 -0500

    controller.js file added

---
+++ controllers/burger_controllers.js
@@ -0,0 +1,33 @@
+var burger = require("../models/burger.js");
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+    burger.selectAll(function (data) {
+        var burgersObject = {
+            burgers: data
+        };
+        console.log(burgersObject);
+        res.render("index", burgersObject);
+    });
+});
+
+router.post("/", (req, res) => {
+
+    if (req.body.burger_name === ''){
+      console.log('not valid')
+    } else {
+      burger.insertOne(req.body.burger_name, function () {
+        res.redirect("/");
+    });
+    }
+});
+
+router.post("/:id", (req, res) => {
+    var id = req.params.id;
+    burger.updateOne(id, function () {
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
