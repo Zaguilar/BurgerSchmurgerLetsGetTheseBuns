906012df9ee522c1dd2b7c5b6d084a2380ba0082
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 18:34:17 -0500

    burger.js orm functions added

---
+++ db/models/burger.js
@@ -0,0 +1,28 @@
+var orm = require ("../config/orm.js")
+
+//orm functions called
+var burger = {
+    selectAll: function (bk) {
+        orm.selectAll(function (res) {
+            bk(res);
+        });
+    },
+    insertOne: function (burger, bk) {
+        orm.insertOne(burger, function (res) {
+            bk(res);
+        });
+    },
+    updateOne: function (id, bk) {
+        orm.updateOne([id], function (res) {
+            bk(res);
+        });
+    },
+    deleteOne: function (id, bk) {
+        orm.deleteOne([id], function (res) {
+            bk(res)
+        })
+    }
+    
+};
+//export burger.js file
+module.exports = burger;
