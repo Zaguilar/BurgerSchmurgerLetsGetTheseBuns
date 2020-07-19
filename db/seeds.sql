21ec56484d3b0e291a3ef5fd415dad4237ea0118
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 20:44:56 -0500

    renaming to fix error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Teriyaki Pineapple Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Hatch Green Chili Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Teriyaki Pineapple Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hatch Green Chili Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", false);
