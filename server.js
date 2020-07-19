d8f28e87e5c346bbdc025021b2ec95ae6174dcf9
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-18 20:45:57 -0500

    modified code to fix pathing error

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 var express = require("express");
+
+var path = require("path");
+
+var hBars = require("express-handlebars");
+
+var routes = require("./controllers/burger_controllers.js");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", hBars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
